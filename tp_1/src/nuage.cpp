938e5aa1d348b364e57654941d847bc02bb428e8
clbourges1 <clbourges1@etud.local.isima.fr>
2018-10-10 15:30:46 +0200

    20 tests

--- tp_1/src/nuage.cpp
+++ tp_1/src/nuage.cpp
@@ -1,5 +1,9 @@
 #include <nuage.hpp>
-int Nuage::size()
+
+
+typedef vector<Point*>::const_iterator const_iterator;
+
+int Nuage::size() const
 {
 	return vect.size();
 }
@@ -12,3 +16,33 @@ void Nuage::ajouter(Polaire p)
 {
 	vect.push_back(&p);
 }
+const_iterator Nuage::begin() const
+{
+	return vect.cbegin();
+}
+
+const_iterator Nuage::end() const
+{
+	return vect.cend();
+}
+
+Cartesien barycentre(Nuage & n) 
+{
+	double x=0,y=0,nb=(double)n.size();
+	Cartesien current(0,0);
+	for(Nuage::const_iterator it = n.begin(); it != n.end();++it)
+	{
+		(*it)->convertir(current);
+		x+=current.getX();
+		y+=current.getY();
+	}
+	if(nb!=0)
+	{
+		current.setX(x/nb);
+		current.setY(y/nb);
+	}
+	return current;
+}
+
+
+
--- tp_1/src/nuage.hpp
+++ tp_1/src/nuage.hpp
@@ -2,6 +2,7 @@
 #define NUA
 #include <point.hpp>
 #include <cartesien.hpp>
+#include <polaire.hpp>
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -9,16 +10,40 @@
 
 using namespace std;
 
-class Point;
-class Cartesien;
-
 class Nuage 
 {
 	public:
-		vector<Point *> vect;
-		int size();
+		vector<Point *>vect;
+		typedef vector<Point*>::const_iterator const_iterator;
+		typedef std::vector<Point*>::iterator iterator;
+		int size()const ;
 		void ajouter(Cartesien c);
 		void ajouter(Polaire p);
+		const_iterator begin() const;
+		const_iterator end() const;
+		iterator begin(){return vect.begin();};
+ 		iterator end(){return vect.end();};
 };
 
+Cartesien barycentre(Nuage & n);
+/*
+class BarycentreCartesien
+{
+	public:
+		typedef vector<Point*>::const_iterator const_iterator;
+		Cartesien operator()(Nuage & n)
+		{
+			cout << "OPERATEUR"<<endl;
+			cout << n.size()<<endl;
+			for(iterator it = n.begin(); it != n.end();++it)
+			{
+				cout << "Coucou"<<endl;
+				Cartesien current(0,0);
+				//(*it)->convertir(current);
+				cout << "Coucou"<<endl;
+				cout << current.x;
+			}
+			return barycentre(n);
+		}
+}; */
 #endif
