f0e8ecc1a528ac7a335119154672d33c7ebc001f
clbourges1 <clbourges1@etud.local.isima.fr>
2018-11-05 14:19:37 +0100

    Test 12

---
+++ tp_3/src/classe.cpp
@@ -0,0 +1,37 @@
+#include <classe.hpp>
+
+Classe::Classe(double a, double b)
+{
+  this->setBorneInf(a);
+  this->setBorneSup(b);
+  quantite=0;
+}
+
+double Classe::getBorneInf()const
+{
+ return borneinf;
+}
+double Classe::getBorneSup()const
+{
+  return bornesup;
+}
+int Classe::getQuantite()const
+{
+  return quantite;
+}
+void Classe::setBorneInf(double x)
+{
+  borneinf=x;
+}
+void Classe::setBorneSup(double x)
+{
+  bornesup=x;
+}
+void Classe::setQuantite(int x)
+{
+  quantite=x;
+}
+void Classe::ajouter()
+{
+  quantite++;
+}
